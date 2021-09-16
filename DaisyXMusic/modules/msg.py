1180090161e8fe23026e20690bfaf637b6fd4356
Cyber Warriors <89768855+Cyber01warriors@users.noreply.github.com>
2021-09-16 02:42:40 -0700

    Update msg.py

--- DaisyXMusic/modules/msg.py
+++ DaisyXMusic/modules/msg.py
@@ -18,7 +18,7 @@ from DaisyXMusic.config import ASSISTANT_NAME, PROJECT_NAME
 
 
 class Messages:
-    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n 😎My master is : @Cyber01_Warriors. \n\n✅ Send me /help for more info."
+    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n😎My master is : @Cyber01_Warriors. \n\n✅ Send me /help for more info."
     HELP_MSG = [
         ".",
         f"""
